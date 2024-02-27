6cf5f446bb2aabb04625b09de305977dafcedb69
Victor S <victor.skarerverket@gmail.com>
2024-02-27 15:58:30 +0100

    Separated code and added a interface.
    Was thinking of ways to showcase my understanding so I decided to try and separate the EmployeeList functions in to a separate class that implements an interface. Not that it's really needed..

---
+++ EmployeeListHandler.cs
@@ -0,0 +1,76 @@
+﻿using PersonalRegisterLexicon2024.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegisterLexicon2024
+{
+    public class EmployeeListHandler : IEmployee
+    {
+        public static List<Models.Employee> employees = new();
+        public void ListEmployees()
+        {
+            Console.WriteLine("|{0,-20}|{1,-20}|{2,-20}|", "Salary", "First Name", "Last Name");
+            Console.WriteLine("|--------------------|--------------------|--------------------|");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("|{0,-20}|{1,-20}|{2,-20}|", employee.Salary, employee.FirstName, employee.LastName);
+
+            }
+        }
+        public void AddEmployee()
+        {
+            Models.Employee employee = new Models.Employee();
+
+            employee.Id = Guid.NewGuid();
+
+            string input;
+            Console.Write("Enter Employee FirstName: ");
+            input = Console.ReadLine();
+            if (input.Length > 20)
+            {
+                Console.WriteLine("First Name is too long!");
+                return;
+            }
+            employee.FirstName = input;
+
+            Console.Write("Enter Employee LastName: ");
+            input = Console.ReadLine();
+            if (input.Length > 20)
+            {
+                Console.WriteLine("Last Name is too long!");
+                return;
+            }
+            employee.LastName = input;
+
+            Console.Write("Enter Employee Salary: ");
+            input = Console.ReadLine();
+            if (input.Length > 20)
+            {
+                Console.WriteLine("Salary is too high!");
+                return;
+            }
+            if (int.TryParse(input, out int result))
+            {
+                employee.Salary = result;
+            }
+            else
+            {
+                Console.WriteLine("Invalid salary input!");
+                return;
+            }
+
+            employees.Add(employee);
+            return;
+        }
+    }
+
+    public interface IEmployee
+    {
+        static List<Models.Employee> employees = new();
+        public void AddEmployee();
+        public void ListEmployees();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 {
     internal class Program
     {
-        // Employee list.
-        static List<Models.Employee> employees = new();
+        
 
         static void Main(string[] args)
         {
+            EmployeeListHandler employeeListHandler = new EmployeeListHandler();
             string input = "";
             while (input != "EXIT")
             {
@@ -18,73 +18,17 @@
                 input = Console.ReadLine();
                 if (input == "1")
                 {
-                    AddEmployee();
+
+                    employeeListHandler.AddEmployee();
+                    
                 }
                 else if (input == "2")
                 {
-                    ListEmployees();
+                    employeeListHandler.ListEmployees();
                 }
                 // case sensitive check.
                 input = input.ToUpper();
             }
-        }
-        public static void AddEmployee()
-        {
-            Models.Employee employee = new Models.Employee();
-
-            employee.Id = Guid.NewGuid();
-
-            string input;
-            Console.Write("Enter Employee FirstName: ");
-            input = Console.ReadLine();
-            if(input.Length > 20) 
-            {
-                Console.WriteLine("First Name is too long!");
-                return;
-            }
-            employee.FirstName = input;
-
-            Console.Write("Enter Employee LastName: ");
-            input = Console.ReadLine();
-            if (input.Length > 20)
-            {
-                Console.WriteLine("Last Name is too long!");
-                return;
-            }
-            employee.LastName = input;
-
-            Console.Write("Enter Employee Salary: ");
-            input = Console.ReadLine();
-            if(input.Length > 20) 
-            {
-                Console.WriteLine("Salary is too high!");
-                return;
-            }
-            if(int.TryParse(input, out int result))
-            {
-                employee.Salary = result;
-            }
-            else
-            {
-                Console.WriteLine("Invalid salary input!");
-                return;
-            }
-
-            employees.Add(employee);
-            return;
-        }
-        public static void ListEmployees()
-        {
-            // Console.WriteLine();
-            Console.WriteLine("|{0,-20}|{1,-20}|{2,-20}|", "Salary", "First Name", "Last Name");
-            Console.WriteLine("|--------------------|--------------------|--------------------|");
-            foreach (var employee in employees)
-            {
-                //Console.WriteLine($"{employee.Salary} {employee.FirstName} {employee.LastName}");
-                Console.WriteLine("|{0,-20}|{1,-20}|{2,-20}|", employee.Salary, employee.FirstName, employee.LastName);
-
-            }
-            Console.WriteLine();
         }
     }
 }
