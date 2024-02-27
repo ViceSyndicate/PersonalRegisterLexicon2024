acb50947ef372cdcf307079c7621e987c84e300f
Victor S <victor.skarerverket@gmail.com>
2024-02-27 14:33:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonalRegisterLexicon2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] wordList = line.Split(' ');
+                if (wordList[0] == "Simon" && wordList[1] == "says")
+                {
+                    wordList[0] = "";
+                    wordList[1] = "";
+
+                    for (int i = 0; i < wordList.Length; i++)
+                    {
+                        Console.Write(wordList[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
